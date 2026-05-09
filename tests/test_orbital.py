6b0695486ae7937e6a50cb752a2e806293489893
Szymon Cichowski <96923331+SzeMrok@users.noreply.github.com>
2026-05-10 01:51:01 +0200

    Test/additional orbital tests (#9)
    * add missing orbital functions tests
    
    * fix expected values

--- tests/test_orbital.py
+++ tests/test_orbital.py
@@ -1,5 +1,6 @@
-from physics_utils.orbital import escape_velocity
+from physics_utils.orbital import escape_velocity, orbital_velocity, orbital_period
 from physics_utils import _si
+import math
 
 
 def test_escape_velocity():
@@ -12,3 +13,27 @@ def test_escape_velocity():
     
     assert abs((calculated_escape_velocity - expected_escape_velocity).magnitude) < 1, \
         f"Expected {expected_escape_velocity} m/s, got {calculated_escape_velocity} m/s"
+        
+
+def test_orbital_velocity():
+    
+    earth_mass = 5.972e24  # kg
+    earth_radius = 6.371e6  # m
+    expected_orbital_velocity = 7910 * _si.meter / _si.second  # m/s
+    
+    calculated_orbital_velocity = orbital_velocity(earth_mass, earth_radius)
+    
+    assert abs((calculated_orbital_velocity - expected_orbital_velocity).magnitude) < 1, \
+        f"Expected {expected_orbital_velocity} m/s, got {calculated_orbital_velocity} m/s"
+        
+
+def test_orbital_period():
+    
+    earth_mass = 5.972e24  # kg
+    earth_radius = 6.371e6  # m
+    expected_orbital_period = 5061 * _si.second  # s
+    
+    calculated_orbital_period = orbital_period(earth_mass, earth_radius)
+    
+    assert abs((calculated_orbital_period - expected_orbital_period).magnitude) < 1, \
+        f"Expected {expected_orbital_period} s, got {calculated_orbital_period} s"

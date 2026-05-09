3d1b221765b93cd249c57cdd810f46af4041495f
Szymon Cichowski <sljcichowski@gmail.com>
2026-05-10 00:25:35 +0200

    add orbital & mechanics functions

---
+++ src/physics_utils/mechanics.py
@@ -0,0 +1,23 @@
+import pint
+
+_si = pint.UnitRegistry()
+
+def kinetic_energy(m: float, v: float) -> float:
+    """
+    Calculate the kinetic energy.
+    
+    Args:
+        m (float): Mass of the object (in kilograms).
+        v (float): Velocity of the object (in meters per second).
+    
+    Returns:
+        float: Kinetic energy (in joules).
+    """
+    
+    if not isinstance(m, _si.Quantity):
+        m = m * _si.kg
+        
+    if not isinstance(v, _si.Quantity):
+        v = v * _si.m / _si.s
+        
+    return 0.5 * m * v**2

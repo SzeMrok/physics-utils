0c303573b2006587b8ec5372560c6c664b35917e
Szymon Cichowski <96923331+SzeMrok@users.noreply.github.com>
2026-05-10 03:16:09 +0200

    version v0.1.0 (#16)
    * add orbital & mechanics functions (#1)
    
    * initialize project structure (#4)
    
    * add physics tests (#5)
    
    * fix existing tests (#6)
    
    * add pyproject.toml contents (#8)
    
    * Test/additional orbital tests (#9)
    
    * add missing orbital functions tests
    
    * fix expected values
    
    * add github actions test workflow (#10)
    
    * Docs/auto docs deployment (#11)
    
    * configure mkdocs documentation
    
    * add github actions docs workflow
    
    * add notebook contents (#12)
    
    * add binder support (#13)
    
    * Notebook/polish (#14)
    
    * clear notebook outputs
    
    * add github actions publish workflow (#15)

---
+++ src/physics_utils/orbital.py
@@ -0,0 +1,68 @@
+import math
+from . import _si
+
+# gravitational constant
+G = 6.67430e-11 * _si.meter**3 / (_si.kilogram * _si.second**2)
+
+
+def escape_velocity(M: float, R: float) -> float:
+    """
+    Calculate the escape velocity.
+    
+    Args:
+        M (float): Mass of the celestial body (in kilograms).
+        R (float): Radius from the center of the celestial body (in meters).
+    
+    Returns:
+    float: Escape velocity (in meters per second).
+    """
+    
+    if not isinstance(M, _si.Quantity):
+        M = M * _si.kg
+        
+    if not isinstance(R, _si.Quantity):
+        R = R * _si.m
+        
+    return (2 * G * M / R) ** 0.5
+
+
+def orbital_velocity(M: float, R: float) -> float:
+    """
+    Calculate the orbital velocity.
+    
+    Args:
+        M (float): Mass of the celestial body (in kilograms).
+        R (float): Radius from the center of the celestial body (in meters).
+    
+    Returns:
+        float: Orbital velocity (in meters per second).
+    """
+    
+    if not isinstance(M, _si.Quantity):
+        M = M * _si.kg
+        
+    if not isinstance(R, _si.Quantity):
+        R = R * _si.m
+        
+    return (G * M / R) ** 0.5
+
+
+def orbital_period(M: float, a: float) -> float:
+    """
+    Calculate the orbital period.
+    
+    Args:
+        M (float): Mass of the celestial body (in kilograms).
+        a (float): Semi-major axis of the orbit (in meters).
+    
+    Returns:
+        float: Orbital period (in seconds).
+    """
+    
+    if not isinstance(M, _si.Quantity):
+        M = M * _si.kg
+        
+    if not isinstance(a, _si.Quantity):
+        a = a * _si.m
+        
+    return 2 * math.pi * (a**3 / (G * M)) ** 0.5
